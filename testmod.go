2b852c06d17445096c6b5d07b757f7cdf1c664cf
Mithun Shashidhara <sashidm@wz.hasbro.com>
2020-05-08 11:16:28 +0530

    name change3

--- testmod.go
+++ testmod.go
@@ -4,6 +4,6 @@ import "fmt"
 
 // Hi returns a friendly greeting
 func Hi(name string) string {
-   return fmt.Sprintf("MSG from golang1: , %s", name)
+   return fmt.Sprintf("MSG from golang1:  %s", name)
 }
 
